99f76d5473732bc68317ec93347efb4755b173bf
Juan Jose Fernandez <fernandez.juan.j@gmail.com>
2020-12-07 21:41:46 -0500

    Adds core chat box design. Evening update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
 
     validates_uniqueness_of :username
+
+    def self.generate
+        adjectives = ['Windswept', 'Monolithic', 'Gleeful', 'Barbaric', 'Glowing', 'Charming', 'Shiny', 'Mysterious']
+        nouns = ['Heath', 'Boulder', 'Imp', 'Troll', 'Dragon', 'Warrior', 'Sorceress', 'Witch', 'Goblin']
+        number = rand.to_s[2..4]
+        username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+        create(username: username)
+    end
 end

29e26a2b6a914a55513de5fbb423b4fc55944e26
Juan Jose Fernandez <51770306+juanjosefernandez@users.noreply.github.com>
2020-12-09 10:50:32 -0500

    Update user.rb
    Adds changes nouns and adjectives.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
     validates_uniqueness_of :username
 
     def self.generate
-        adjectives = ['Windswept', 'Monolithic', 'Gleeful', 'Barbaric', 'Glowing', 'Charming', 'Shiny', 'Mysterious']
-        nouns = ['Heath', 'Boulder', 'Imp', 'Troll', 'Dragon', 'Warrior', 'Sorceress', 'Witch', 'Goblin']
+        adjectives = ['Tricky', 'Huge', 'Gleeful', 'Barbaric', 'Glowing', 'Charming', 'Shiny', 'Mysterious']
+        nouns = ['Knight', 'Skeleton', 'Imp', 'Troll', 'Dragon', 'Warrior', 'Sorceress', 'Witch', 'Goblin']
         number = rand.to_s[2..4]
         username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
         create(username: username)
